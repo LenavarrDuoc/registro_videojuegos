3c69b05cf6803308688e2517311d1fee5419d99a
LenavarrDuoc <leo.navarro@duocuc.cl>
2025-06-06 10:36:59 -0400

    Paso 3: Visualización de videojuegos.

---
+++ videojuegos.py
@@ -0,0 +1,58 @@
+# Código videojuegos con uso de listas y diccionarios.
+
+videojuegos = []
+
+plataformas = ("PC", "PS5", "Xbox Series X/S", "Nintendo Switch")
+
+mani_menu = """--- MENÚ DE VIDEOJUEGOS
+1. Registrar videojuego.
+2. Ver videojugos.
+3. Modificar videojuego.
+4. Eliminar videojuego.
+5. Salir."""
+
+while True:
+    print(mani_menu)
+
+    opt = input("Seleccione una opción (1-5): ")
+
+    if opt == "1":
+        codigo = int(input("Ingrese el código del videojuego: "))
+        nombre = input("Ingrese el nombre del videojuego: ")
+        genero = input("Ingrese el género del videojuego: ")
+
+        print("Plataformas disponible:")
+        print("1. PC")
+        print("2. PS5")
+        print("3. Xbox Series X/S")
+        print("4. Nintendo Swtich")
+
+        plataforma_codigo = int(input("Seleccione el núnmero de la plartaforma: "))
+        plataforma = plataformas[plataforma_codigo - 1]
+
+        videojuego = {
+            "codigo" : codigo,
+            "nombre" : nombre,
+            "genero" : genero,
+            "plataforma" : plataforma
+        }
+
+        videojuegos.append(videojuegos)
+        print("Videojuego registrado correctamente.")
+
+    elif opt == "2":
+        if len(videojuegos) == 0:
+            print("No hay videojuegos registrados.")
+        else:
+            print("\n--- LISTA DE VIDEOJUEGOS ---")
+            for v in videojuegos:
+                print(f"Código: {v['codigo']} | Nombre: {v['nombre']} | Género: {v['genero']} | Plataforma: {v['plataforma']}")
+    elif opt == "3":
+        pass
+    elif opt == "4":
+        pass
+    elif opt == "5":
+        print("Saliendo del programa.")
+        break
+    else:
+        print("ERROR: Opción incorrecta. Debe indicar un número entero desde las opciones disponibles en el menú.")
